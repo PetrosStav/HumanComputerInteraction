ea30aef9f5d110684852eae07588d0e9145b4b8c
sotir <sotiriskot9@gmail.com>
2018-12-07 00:18:09 +0200

    Try it out..added select program activity...the images will appear with a grid view which will use a custom adapter->cloth image adapter...must create a class cloth_image which will populate the grid view

--- app/src/main/java/aueb/hci/humancomputerinteraction/HomeScreen.java
+++ app/src/main/java/aueb/hci/humancomputerinteraction/HomeScreen.java
@@ -1,7 +1,10 @@
 package aueb.hci.humancomputerinteraction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeScreen extends AppCompatActivity {
 
@@ -9,5 +12,21 @@ public class HomeScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_screen);
+        Button select_program = findViewById(R.id.btSelectProgram);
+        Button advanced_program = findViewById(R.id.btAdvancedProgram);
+        Button manage_programs = findViewById(R.id.btManagePrograms);
+        Button help = findViewById(R.id.btHelp);
+        Button start = findViewById(R.id.btStart);
+        Button start_dryer = findViewById(R.id.btStartDryer);
+
+        select_program.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeScreen.this,select_program.class);
+                startActivityForResult(intent,RESULT_CANCELED);
+            }
+        });
     }
+
+
 }
