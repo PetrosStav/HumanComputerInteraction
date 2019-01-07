77be30c9f5b1d17262b4753a97548931b70d3bd7
sotir <sotiriskot9@gmail.com>
2019-01-07 23:58:33 +0200

    Changed the names of the programs for better looks

--- app/src/main/java/aueb/hci/humancomputerinteraction/DAO/Initializer.java
+++ app/src/main/java/aueb/hci/humancomputerinteraction/DAO/Initializer.java
@@ -16,8 +16,8 @@ public abstract class Initializer {
         ProgramDAO programDAO = getProgramData();
 
         Program p1 = new Program();
-        p1.setName("P1");
-        p1.setDescription("Program 1 Description");
+        p1.setName("Default Program 1");
+        p1.setDescription("Default Program 1 Description");
         p1.setFavorited(true);
         p1.setDryer(true);
         p1.setTIME(60);
@@ -25,16 +25,16 @@ public abstract class Initializer {
         p1.setTEMP(45);
 
         Program p2 = new Program();
-        p2.setName("P2");
-        p2.setDescription("Program 2 Description");
+        p2.setName("Default Program 2");
+        p2.setDescription("Default Program 2 Description");
         p2.setFavorited(false);
         p2.setTIME(20);
         p2.setRPM(600);
         p2.setTEMP(45);
 
         Program p3 = new Program();
-        p3.setName("Program 3");
-        p3.setDescription("Program 3 Description");
+        p3.setName("Default Program 3");
+        p3.setDescription("Default Program 3 Description");
         p3.setFavorited(true);
         p3.setDryer(true);
         p3.setTIME(20);
@@ -42,16 +42,16 @@ public abstract class Initializer {
         p3.setTEMP(75);
 
         Program p4 = new Program();
-        p4.setName("Program 4");
-        p4.setDescription("Program 4 Description");
+        p4.setName("Default Program 4");
+        p4.setDescription("Default Program 4 Description");
         p4.setFavorited(false);
         p4.setTIME(80);
         p4.setRPM(1000);
         p4.setTEMP(90);
 
         Program p5 = new Program();
-        p5.setName("Program 5");
-        p5.setDescription("Program 5 Description");
+        p5.setName("Default Program 5");
+        p5.setDescription("Default Program 5 Description");
         p5.setFavorited(true);
         p5.setDryer(true);
         p5.setTIME(40);
@@ -60,7 +60,7 @@ public abstract class Initializer {
 
         Program p6 = new Program();
         p6.setName("Program for white clothes and easy stains");
-        p6.setDescription("Program 6 Description");
+        p6.setDescription("Program for white clothes and easy stains Description");
         p6.setFavorited(true);
         p6.setDryer(true);
         p6.setTIME(60);
